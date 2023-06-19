96f249e25168ad112a91446b9cce8ddd29a6e232
Nathan Stanford <Nathan.Stanford@gmail.com>
2023-06-19 14:55:52 -0400

    updated to a custom css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Tasks</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
+    <link rel="stylesheet" href="/css/bootstrap.css">
     <livewire:styles />
   </head>
   <body>
