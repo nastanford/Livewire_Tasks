ee83418819646b1b1d21a78cb1154ae04a02d276
Nathan Stanford <Nathan.Stanford@gmail.com>
2023-06-19 15:01:33 -0400

    removed comments

--- app/Http/Livewire/Tasks.php
+++ app/Http/Livewire/Tasks.php
@@ -6,14 +6,12 @@ use Livewire\Component;
 use Livewire\WithPagination;
 use App\Models\Task;
 
-// use App\Models\Task as Task;
 
 class Tasks extends Component
 {
   use WithPagination;
   protected $paginationTheme = 'bootstrap';
 
-  // public $tasks = [];
   public $newTask = '';
 
   public function render()
@@ -21,26 +19,12 @@ class Tasks extends Component
     $tasks = Task::paginate(3);
     return view('livewire.tasks', compact('tasks'));
   }
-  // public function render()
-  // {
-  //   // $tasks = $this->tasks = \App\Models\Task::paginate(3);
-  //   // return view('livewire.tasks');
-  //   $this->tasks = Task::all();
-
-  //   // $tasks = Task::paginate(3);
-  //   return view('livewire.tasks');
-  // }
-
   public function addTask()
   {
     $table = new Task;
     $table->task_name = $this->newTask;
     $table->save();
 
-    // Get all of the tasks
-    // $this->reset("task");
-    // $this->tasks = \App\Models\Task::all();
-    // reset the newTask input to null
     $this->newTask = '';
   }
 
@@ -49,29 +33,11 @@ class Tasks extends Component
     $table = Task::find($id);
     $table->is_complete = !$table->is_complete;
     $table->save();
-    // Get all of the tasks
-    // $this->reset("task");
-    // $this->tasks = \App\Models\Task::all();
   }
 
   public function deleteTask($id)
   {
     $table = Task::find($id);
     $table->delete();
-    // Get all of the tasks
-    // $this->reset("task");
-    // $this->tasks = \App\Models\Task::all();
   }
 }
-/*
-
-  public function mount()
-  {
-    // $tasks = Task::paginate(3);
-    // how to paginate tasks
-    // $this->tasks = \App\Models\Task::paginate(3);
-    // $this->reset("tasks");
-    $this->tasks = Task::all();
-    // $this->tasks = Task::paginate(3);
-  }
-*/
